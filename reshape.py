98cbcb4931c7d4bb644cada7227c407bd6e5e5ef
Philippe Martinet <phi.mar.email@gmail.com>
2018-05-18 13:08:25 +0200

    Clean repository -> GitHub

--- reshape.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-#a = np.array([[0,1,0,0,0,1],[1,0,1,0,1,1]])
-a = [[0,1,0,0,0,1],[1,0,1,0,1,1]]
-def doit(a):
-    return ''.join(map(str,a))
-
-for r in range(0,2):
-    print(doit(a[r]))
